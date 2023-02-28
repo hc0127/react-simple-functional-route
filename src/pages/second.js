30c0dcc27dc9e1ac92f9c931d2607bee9984cd67
Roman Shalak <shaksev@gmail.com>
2023-02-28 04:07:07 +0100

    clean up

--- src/pages/second.js
+++ src/pages/second.js
@@ -1,55 +1,17 @@
-import React, { Component } from "react";
-import "../App.css";
+import React, { useEffect } from 'react'; 
 import {
   MDBContainer,
-  MDBRow,
-  MDBCol,
-  MDBBtn,
-  MDBCard,
-  MDBCardBody,
-  MDBCardTitle,
-  MDBTextArea,
-  MDBInput ,
+  MDBBtn
 } from "mdb-react-ui-kit";
 
-class Second extends Component {
-  constructor(props) {
-    super(props);
-  }
+export default function Second(props) {
 
-  render() {
-    return (
-      <MDBContainer className="">
-        <MDBRow className="d-grid gap-6 p-6">
-          <MDBCard>
-            <MDBCardBody>
-              <MDBCardTitle className="text-center">
-                ADVERTISE PROJECT
-              </MDBCardTitle>
-              <MDBRow className="mt-2">
-                <MDBInput label='Title'type='text'/>
-              </MDBRow>
-              <MDBRow className="mt-2">
-                <MDBTextArea label='Description'  rows={10}></MDBTextArea>
-                </MDBRow>
-              <MDBRow className="mt-2">
-                <MDBInput label='Link'type='text'/>
-              </MDBRow>
-              <MDBRow className="mt-2">
-                <MDBRow className="mt-2" >NUMBER OF AD CLICKS:</MDBRow>
-                </MDBRow>
-              <MDBRow className="mt-2">
-                <MDBInput label='TOTAL ETH TO PAY' type='text'/>
-              </MDBRow>
-              <MDBRow className="mt-2">
-                <MDBBtn>DEPLOY ADVERTISMENT</MDBBtn>
-              </MDBRow>
-            </MDBCardBody>
-          </MDBCard>
-        </MDBRow>
-      </MDBContainer>
-    );
-  }
+  useEffect(() => {
+  }, []);
+
+  return (
+    <MDBContainer>
+      <MDBBtn>Click me</MDBBtn>
+    </MDBContainer>
+  );
 }
-
-export default Second;
